6cde03b2988f33bb5db64812fc75b596dca0f0d2
Cameron Smith <camerosmit@yahoo.com>
2018-04-04 21:34:50 -0700

    Added test scenerios for scoring

---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster {
+
+	public enum Action { Tidy, Reset, EndTurn, EndGame };
+
+	public Action Bowl(int pins)
+	{
+		if(pins < 0 || pins > 10)
+		{
+			throw new UnityException("Incorrect number of pins. Must be between 0 and 10");
+		}
+
+		if (pins == 10)
+		{
+			return Action.EndTurn;
+		}
+
+		throw new UnityException("Not sure what action to return!");
+	}
+}
