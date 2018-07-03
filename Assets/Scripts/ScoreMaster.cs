9383fed27b89b1a77119c7fd75dfdc4b556d77f8
Cameron Smith <camerosmit@yahoo.com>
2018-07-02 19:06:10 -0700

    Added a base ScoreMaster.

---
+++ Assets/Scripts/ScoreMaster.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMaster {
+
+	public static List<int> ScoreFrames (List<int> rolls)
+	{
+		List<int> frameList = new List<int>();
+
+		// your code here
+
+		return frameList;
+	}
+}
