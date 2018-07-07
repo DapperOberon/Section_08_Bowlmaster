a8aebbf54bf36af0e67b4048a558e3676b87785c
Cameron Smith <camerosmit@yahoo.com>
2018-07-06 17:43:09 -0700

    Scoring finished.

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -4,12 +4,46 @@ using UnityEngine;
 
 public class ScoreMaster {
 
+	public static List<int> ScoreCumulative(List<int> rolls)
+	{
+		List<int> scoreTotal = new List<int>();
+		int runningTotal = 0;
+
+		foreach(int frameScore in ScoreFrames(rolls))
+		{
+			runningTotal += frameScore;
+			scoreTotal.Add(runningTotal);
+		}
+
+		return scoreTotal;
+	}
+
 	public static List<int> ScoreFrames (List<int> rolls)
 	{
-		List<int> frameList = new List<int>();
+		List<int> frames = new List<int>();
 
-		// your code here
+		// Index i points to 2nd bowl of frame
+		for (int i = 1; i < rolls.Count; i += 2)
+		{
+			if(frames.Count == 10) { break; }						// Prevents 11th frame
 
-		return frameList;
+			if (rolls[i - 1] + rolls[i] < 10)						// Normal "open" frame
+			{ 
+				frames.Add(rolls[i - 1] + rolls[i]);
+			}
+
+			if(rolls.Count - i <= 1) { break; }						// Not enough lookahead
+
+			if(rolls[i-1] == 10)									// Strike Bonus
+			{
+				i--;												// Reset i to first bowl becuase strike only has 1
+				frames.Add(10 + rolls[i + 1] + rolls[i + 2]);
+			} else if (rolls[i - 1] + rolls[i] == 10)				// Spare Bonus
+			{
+				frames.Add(10 + rolls[i+1]);
+			}
+		}
+
+		return frames;
 	}
 }
